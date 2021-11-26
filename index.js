bd410bcf87a3d3d6daa404227571393e276d5d2a
maximilianjones <maxcharles500@gmail.com>
2021-11-26 10:52:32 -0800

    commit tree 7c6728b66795b652a60f5e637b16d74380f35c12

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+
+  /*function superbowlWin(arr) {
+    if (arr.result === "W") {
+        return arr.year
+    }
+    else {
+        return undefined
+    }
+}
+
+record.find(superbowlWin)*/
+
+const superbowlWin = (record) => {
+    const firstWin = record.find(season => {
+        return season.result === 'W'
+
+    })
+    return !! firstWin ? firstWin.year : undefined
+} 
